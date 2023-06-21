8b5632334de148e70ed0e222646929bddcffcb39
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-06-22 00:09:23 +0700

    FirgureAI logic

--- Beat/Assets/EnemyControler.cs
+++ Beat/Assets/EnemyControler.cs
@@ -1,24 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyControler : MonoBehaviour
 {
-    public float damageAmount = 10f;
-    // Amount of damage inflicted on the player
+    public Transform playerPosition; // Reference to the player's position
+    public NavMeshAgent enemy; // Reference to the NavMeshAgent component
+    public Animator anim;
+
+    private bool isIdle = true;
+    private bool foundPlayer = false;
+
+    private void Awake()
+    {
+        enemy = GetComponent<NavMeshAgent>(); // Assign the NavMeshAgent component
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (playerPosition != null)
+        {
+            enemy.SetDestination(playerPosition.position); // Set the destination to the player's position
+        }
+        else
+        {
+            Debug.LogError("Player position is not set!");
+        }
+
+        if (foundPlayer)
+        {
+            Attack();
+        }
+        else if (isIdle)
+        {
+            Idle();
+        }
+        else
+        {
+            Run();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            PlayerControl_Animation player = other.gameObject.GetComponent<PlayerControl_Animation>();
-
-            if (player != null)
-            {
-                // Call the TakeDamage method on the player to inflict damage
-                player.TakeDamage(damageAmount);
-                Debug.Log("Player damaged by enemy! Damage Amount: " + damageAmount);
-            }
+            foundPlayer = true;
+            isIdle = false;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foundPlayer = false;
+            isIdle = true;
+        }
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    private void Run()
+    {
+        anim.SetFloat("Speed", enemy.velocity.magnitude);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0f);
     }
 }
